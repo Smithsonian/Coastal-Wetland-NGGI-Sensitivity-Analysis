3a10ec6b63e3900192fdf78cd776f88399fb8919
HolmquistJ <HolmquistJ@si.edu>
2017-10-16 23:38:33 -0400

    Updated Mangrove Biomass Curve.

--- MangroveBiomassCurve.R
+++ MangroveBiomassCurve.R
@@ -4,30 +4,31 @@ mengBiomass <- read.csv(paste(getwd(), "/data/MengReview/mangroveAndMarshesABG.c
 mangroveBiomass <- subset(mengBiomass , Ecosystem == "mangrove")
 mangroveBiomass <- subset(mangroveBiomass, (AGBunits != 'gC_m2') & (AGBunits != 'MgC_ha'))
 
-convertBiomassUnits <- function(input_measurement, input_unit, c_conversion=0.5) {
+convertBiomassUnits <- function(input_measurement, input_unit, c_conversion=0.42) {
   gPerMg = 1000000
   m2PerHa = 10000
   gPerKg = 1000
   if (input_unit == "g_m2") { return(input_measurement) }
-  else if (input_unit == "gC_m2") { return(input_measurement / c_conversion) }
-  else if (input_unit == "MgC_ha") { return(input_measurement / c_conversion / m2PerHa * gPerMg) }
-  else if (input_unit == "Mg_ha") { return(input_measurement / m2PerHa * gPerMg) }
-  else if (input_unit == "kg_ha") { return(input_measurement / m2PerHa * gPerKg) }
+  else if (input_unit == "gC_m2") { return(input_measurement) }
+  else if (input_unit == "MgC_ha") { return(input_measurement / m2PerHa * gPerMg) }
+  else if (input_unit == "Mg_ha") { return(input_measurement * c_conversion / m2PerHa * gPerMg) }
+  else if (input_unit == "kg_ha") { return(input_measurement * c_conversion / m2PerHa * gPerKg) }
   else {
     print('error. Cannot convert')
     return(NA)
     }
 }
 
-AGBgm2 <- mapply(convertBiomassUnits, mangroveBiomass$AGB, mangroveBiomass$AGBunits)
+AGBgCm2 <- mapply(convertBiomassUnits, mangroveBiomass$AGB, mangroveBiomass$AGBunits)
 
-mangroveBiomass["AGMgPerM2"] <- AGBgm2
+mangroveBiomass["AGBgCm2"] <- AGBgCm2
 
-hist(log(mangroveBiomass$AGMgPerM2))
-hist(mangroveBiomass$AGMgPerM2)
+hist(log(mangroveBiomass$AGBgCm2))
+hist(mangroveBiomass$AGBgCm2)
 
 (logMeanMangrove <- mean(log(mangroveBiomass$AGMgPerM2)))
 (logSdMangrove <- sd(log(mangroveBiomass$AGMgPerM2)))
+(exp(logMeanMangrove))
 (exp(logMeanMangrove+logSdMangrove))
 (exp(logMeanMangrove-logSdMangrove))
 
