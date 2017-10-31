6b66b0b0745bda69d70693659d2335a2bbf3ccaa
HolmquistJ <HolmquistJ@si.edu>
2017-10-31 13:28:09 -0400

    Improved Palustrine Below MHHWS Figure

--- MHHWS probability in PEMFOSS.R
+++ MHHWS probability in PEMFOSS.R
@@ -1,5 +1,5 @@
 library(foreign)
-mhhws_pal <- read.dbf(paste(getwd(), "/data/pMHHWSforCCAPpal.dbf", sep=""))
+mhhws_pal <- read.dbf(paste(getwd(), "/data/WetlandArea/Palustrine/pMHHWSforCCAPpal.dbf", sep=""))
 
 # using poisson Binomial Distrubution Normal Approximation Method (Hong, 2013)
 value <- mhhws_pal$Value / 10000 # tranform values
@@ -27,9 +27,16 @@ sigma_ha <- sigma * 900 / 10000
 (sigma_mHa <- sigma_ha / 1000000)
 
 # plot distribution of total pixels
-target_x <-seq(mu - (5*sigma), mu + (5*sigma), by = 100)
+target_x <-seq(mu - (4*sigma), mu + (4*sigma), by = 1)
+target_x.mha <- target_x * 900 / 10000 / 1E6
 target_y <- c()
 for (j in 1:length(target_x)) { target_y <- c(target_y, dnorm(target_x[j], mu, sigma)) }
 
-plot(target_x, target_y, type="l", xlab="Pixels (n)", ylab="probability density", main="Palustrine Wetlands < MHHWS (2006-2010)")
-
+par(mar=c(1,1,1,1), oma=c(3,3,3,1))
+plot(target_x.mha, target_y, type="l", xlab="Pixels (n)", ylab="probability density", main=expression(paste("Palustrine Wetlands < MHHWS")), lwd=2, axes=F)
+axis(1)
+axis(2)
+#box()
+mtext("Area (ha)", side=1, line=1.5, outer=T)
+mtext("Density", side=2, line=1.5, outer=T)
+mtext(expression(paste(mu, "=", sum(paste(n[i], phi[i]), paste("i=", 0), 1), "   ", sigma^2, "=",  sum(paste(n[i], phi[i], "(1-", phi[i],")"), paste("i=", 0), 1))), side=3, line=1.5)
